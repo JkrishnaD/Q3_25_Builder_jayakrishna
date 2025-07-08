4342030757c630662294f22009b65a5809255001
JKrishnaD <jayak5063@gmail.com>
2025-07-09 00:43:25 +0530

    expect vaules

--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Vault } from "../target/types/vault";
+import { expect } from "chai";
 
 describe("vault", () => {
   // Configure the client to use the local cluster.
@@ -10,6 +11,16 @@ describe("vault", () => {
   const program = anchor.workspace.vault as Program<Vault>;
 
   const user = provider.wallet;
+
+  const accountSpace = 8 + 1 + 1; // discriminator + vault_bump + state_bump
+
+  let rentExemptLamports: number;
+
+  before(async () => {
+    rentExemptLamports = await provider.connection.getMinimumBalanceForRentExemption(0);
+
+    console.log(`Rent-exempt amount for ${accountSpace} bytes:`, rentExemptLamports / anchor.web3.LAMPORTS_PER_SOL, "SOL");
+  })
 
   const vaultState = anchor.web3.PublicKey.findProgramAddressSync(
     [Buffer.from("state"), user.publicKey.toBuffer()],
@@ -36,6 +47,10 @@ describe("vault", () => {
     console.log("Your transaction signature", tx);
     const account_info = await provider.connection.getAccountInfo(vault);
     console.log("account info", account_info);
+
+    // as the account initiated we need to have rent in the account
+    expect(account_info).not.to.be.null
+    expect(account_info.lamports).to.be.equal(rentExemptLamports)
   });
 
   it("Is money deposited", async () => {
@@ -51,6 +66,10 @@ describe("vault", () => {
     console.log("Your transaction signature", tx);
     const account_info = await provider.connection.getAccountInfo(vault);
     console.log("account info", account_info);
+
+    const expected_amounte = rentExemptLamports + 2 * anchor.web3.LAMPORTS_PER_SOL;
+    // expected amount is to be 2 sol we added and the rent amount
+    expect(account_info.lamports).to.be.equal(expected_amounte)
   });
 
   it("Is money withdrawn", async () => {
@@ -66,6 +85,9 @@ describe("vault", () => {
     console.log("Your transaction signature", tx);
     const account_info = await provider.connection.getAccountInfo(vault);
     console.log("your account info", account_info)
+
+    // as the 2 sol is withdrawn we have just the rent excempt sol
+    expect(account_info.lamports).to.be.equal(rentExemptLamports);
   });
 
   it("Is account closed", async () => {
@@ -74,9 +96,12 @@ describe("vault", () => {
       vault,
       vaultState
     }).rpc();
-    
+
     console.log("Your transaction signature", tx);
     const account_info = await provider.connection.getAccountInfo(vault);
     console.log("your account info", account_info)
+
+    //here the account is closed so there will be not account which is null
+    expect(account_info).to.be.null
   })
 });
