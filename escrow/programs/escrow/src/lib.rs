c430221e1df851336b04e675ed1d0142f72cf9a5
JKrishnaD <jayak5063@gmail.com>
2025-07-10 00:30:51 +0530

    done contract

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -5,14 +5,41 @@ use anchor_lang::prelude::*;
 pub mod instructions;
 pub mod state;
 
+pub use instructions::*;
+
 declare_id!("CwgCa5b6vwB5DLRdtAc8rQ9gFhqUvBEoT5mk1Uh3zLWd");
 
 #[program]
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Make>,seed:u64,recieve:u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, recieve, &ctx.bumps)?;
         msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn maker_deposite(ctx: Context<Make>,amount:u64)->Result<()>{
+        ctx.accounts.deposit(amount)?;
+        msg!("Maker deposited {} tokens", amount);
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>)->Result<()>{
+        ctx.accounts.refund_and_close()?;
+        msg!("Refunded and closed escrow account");
+        Ok(())
+    }
+
+    pub fn taker_deposite(ctx:Context<Take>,amount:u64)->Result<()>{
+        ctx.accounts.deposite(amount)?;
+        msg!("Taker deposited {} tokens", amount);
+        Ok(())
+    }
+
+    pub fn taker_take_and_close(ctx:Context<Take>)->Result<()>{
+        ctx.accounts.take_and_close()?;
+        msg!("Taker took and closed escrow account");
         Ok(())
     }
 }
