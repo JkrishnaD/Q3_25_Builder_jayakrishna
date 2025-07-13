5b92a03cb7f20ef7a2bf27a6e54e07536b044b67
JKrishnaD <jayak5063@gmail.com>
2025-07-13 22:33:26 +0530

    added events

---
+++ amm/programs/amm/src/events.rs
@@ -0,0 +1,62 @@
+use anchor_lang::prelude::*;
+
+#[event]
+pub struct InitializeEvent {
+    pub admin: Pubkey,
+    pub mint_x: Pubkey,
+    pub mint_y: Pubkey,
+    pub mint_lp: Pubkey,
+    pub vault_x: Pubkey,
+    pub vault_y: Pubkey,
+    pub config: Pubkey,
+    pub fee: u16,
+}
+
+#[event]
+pub struct DepositEvent {
+    pub user: Pubkey,
+    pub mint_x: Pubkey,
+    pub mint_y: Pubkey,
+    pub mint_lp: Pubkey,
+    pub vault_x: Pubkey,
+    pub vault_y: Pubkey,
+    pub user_lp: Pubkey,
+    pub config: Pubkey,
+    pub max_x: u64,
+    pub max_y: u64,
+    pub amount: u64,
+}
+
+#[event]
+pub struct SwapEvent {
+    pub user: Pubkey,
+    pub mint_x: Pubkey,
+    pub mint_y: Pubkey,
+    pub mint_lp: Pubkey,
+    pub vault_x: Pubkey,
+    pub vault_y: Pubkey,
+    pub user_lp: Pubkey,
+    pub config: Pubkey,
+    pub amount: u64,
+    pub min: u64,
+}
+
+pub struct WithdrawEvent {
+    pub user: Pubkey,
+    pub mint_x: Pubkey,
+    pub mint_y: Pubkey,
+    pub mint_lp: Pubkey,
+    pub vault_x: Pubkey,
+    pub vault_y: Pubkey,
+    pub user_lp: Pubkey,
+    pub config: Pubkey,
+    pub amount: u64,
+    pub min_x: u64,
+    pub min_y: u64,
+}
+
+#[event]
+pub struct UpdateEvent {
+    pub user: Pubkey,
+    pub config: Pubkey,
+}
